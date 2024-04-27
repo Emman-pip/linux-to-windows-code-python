24446c7b80d8ae79059425fbb57f7cbb09b37533
Emman-pip <parrenoemmanuel756@gmail.com>
2024-04-28 06:19:08 +0800

    Make a Flask boilerplate.

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
-from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase
 
-print(datetime.now())
+
+class base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=base)
+
+
+class SampleEntity(db.Model):
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+
+db.init_app(app)
